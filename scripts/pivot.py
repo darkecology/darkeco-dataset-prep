0a89d679ddd265d387cc334cb20eabffc3ff3a29
Dan Sheldon <sheldon@cs.umass.edu>
2022-01-06 14:57:31 -0500

    Add pivot script to summarize across station and create time series with columns corresponding to select variables and stations

---
+++ scripts/pivot.py
@@ -0,0 +1,72 @@
+import argparse
+import pandas as pd
+import os
+import glob
+from tqdm import tqdm
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Aggregate by station and "pivot" so columns correspond to stations'
+    )    
+
+    parser.add_argument('--root', help='data root directory (default: ../data)', default='../data')
+    parser.add_argument('--years', nargs="+", type=int, help="years to process")
+    parser.add_argument('--min', help="pivot 5min data", dest='do_5min', action='store_true')
+    parser.add_argument('--no-5min', help="don't pivot 5min data", dest='do_5min', action='store_false')
+    parser.add_argument('--daily', help="pivot daily data", dest='do_daily', action='store_true')
+    parser.add_argument('--no-daily', help="don't pivot daily data", dest='do_daily', action='store_false')
+
+    parser.set_defaults(do_5min=True, do_daily=True)
+    args = parser.parse_args()
+    
+    def read_files(files):
+        for file in tqdm(files):
+            yield pd.read_csv(file)
+
+    root = args.root
+    if not os.path.exists(root):
+        raise FileNotFoundError(f'Path {root} does not exist')
+
+    for year in args.years:
+        print(f'***{year}***')
+        if args.do_5min:
+            print(' * 5-minute')
+            files = glob.glob(f'{root}/5min/????-{year}-5min.csv')
+
+            print('   - reading files')
+            df = pd.concat(read_files(files))
+            
+            df = df.pivot(index=['date'],
+                          columns='station',
+                          values=['density', 'traffic_rate', 'u', 'v', 'percent_rain'])
+            
+            print('   - writing output')
+            outdir = f'{root}/allstations/5min'
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)            
+            outfile = f'{outdir}/{year}-5min.csv'
+            df.to_csv(outfile)
+            
+        if args.do_daily:
+            print(' * Daily')
+            files = glob.glob(f'{root}/daily/????-{year}-daily.csv')
+            
+            print('   - reading files')
+            df = pd.concat(read_files(files))
+                
+            df = df[df['percent_missing'] <= 0.2]
+            
+            df = df.pivot(index=['date', 'period'],
+                          columns='station',
+                          values=['density_hours', 'u', 'v', 'percent_rain'])
+
+            print('   - writing output')
+            outdir = f'{root}/allstations/daily'
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)            
+            outfile = f'{outdir}/{year}-daily.csv'
+            df.to_csv(outfile)
+
+if __name__ == "__main__":
+    main()
--- scripts/summarize.py
+++ scripts/summarize.py
@@ -7,6 +7,7 @@ from nexrad import get_lat_lon
 import pvlib
 import time as t
 import warnings
+from tqdm import tqdm
 
 import util
 
@@ -20,6 +21,7 @@ def main():
     parser.add_argument('--stations',  nargs="+", help='stations to process')
     parser.add_argument('--years', nargs="+", type=int, help="years to process")
     parser.add_argument('--max_scans', type=int, default=None)    
+    parser.add_argument('--scans_chunk_size', type=int, default=500)
     parser.add_argument('--actions', nargs="+", choices=['scan', 'resample', 'daily', 'all'], default='all')
     
     args = parser.parse_args()
@@ -28,16 +30,26 @@ def main():
     resample_frequency = '5min'
     
     if 'scan' in actions or 'all' in actions: # True if actions is either 'all' or ['all']
-        aggregate_station_years_by_scan(args.root, args.stations, args.years, args.max_scans)
+        aggregate_station_years_by_scan(args.root,
+                                        args.stations,
+                                        args.years,
+                                        args.max_scans,
+                                        chunk_size=args.scans_chunk_size)
 
     if 'resample' in actions or 'all' in actions:
-        resample_station_years(args.root, args.stations, args.years, freq=resample_frequency)
+        resample_station_years(args.root,
+                               args.stations,
+                               args.years,
+                               freq=resample_frequency)
 
     if 'daily' in actions or 'all' in actions:
-        aggregate_station_years_to_daily(args.root, args.stations, args.years, freq=resample_frequency)
+        aggregate_station_years_to_daily(args.root,
+                                         args.stations,
+                                         args.years,
+                                         freq=resample_frequency)
 
 
-def aggregate_station_years_by_scan(root, stations, years, max_scans):
+def aggregate_station_years_by_scan(root, stations, years, max_scans, chunk_size=100):
 
     if not os.path.exists(root):
         raise FileNotFoundError(f'Path {root} does not exist')
@@ -64,9 +76,9 @@ def aggregate_station_years_by_scan(root, stations, years, max_scans):
     for station in stations:
         for year in years:
 
-            station_year = f"{station}-{year}"
-            print(station_year)
-            
+            print(f"***Aggregate by scan***")
+
+            station_year = f"{station}-{year}"            
             path = f"{file_list_folder}/{station}-{year}.txt"            
             if path not in paths:
                 warnings.warn(f"{path} not found")
@@ -76,44 +88,48 @@ def aggregate_station_years_by_scan(root, stations, years, max_scans):
                 profile_paths = infile.read().split('\n')
 
             start = t.time()
-            outfile = f'{root}/scan_level/{station_year}.csv'
+
+            # Get rows from individual files
+            if max_scans and len(profile_paths) > max_scans - num_scans:
+                profile_paths = profile_paths[:max_scans-num_scans]                
+
+            num_scans += len(profile_paths)
+
+            def read_chunks():
+                print(f"{station}-{year}, chunks of {chunk_size}")
+                for pos in tqdm(range(0, len(profile_paths), chunk_size)):
+                    yield util.aggregate_profiles_to_scan_level(
+                        profile_paths[ pos : pos + chunk_size ],
+                        lat_lon
+                    )
+                    
+            df = pd.concat(read_chunks())
+                                 
+            # Add solar elevation (note: much faster to do in batch at end than row-by-row)
+            solar_elev = pvlib.solarposition.spa_python(df['date'], df['lat'], df['lon'])
+            df['solar_elevation'] = solar_elev['elevation'].values
+            
+            # Convert lat/lon to strings to preserve full precision --- other floats will be truncated
+            df['lat'] = df['lat'].apply(str)
+            df['lon'] = df['lon'].apply(str)
 
             column_names = ['station',                    # 0
                             'lat',                        # 1
                             'lon',                        # 2
                             'date',                       # 3
-                            'density',                    # 4
-                            'density_precip',             # 5
-                            'traffic_rate',               # 6
-                            'traffic_rate_precip',        # 7
-                            'u',                          # 8
-                            'v',                          # 9
-                            'speed',                      # 10
-                            'direction',                  # 11
-                            'percent_rain',               # 12
-                            'rmse']                       # 13
+                            'solar_elevation',            # 4
+                            'density',                    # 5
+                            'density_precip',             # 6
+                            'traffic_rate',               # 7
+                            'traffic_rate_precip',        # 8
+                            'u',                          # 9
+                            'v',                          # 10
+                            'speed',                      # 11
+                            'direction',                  # 12
+                            'percent_rain',               # 13
+                            'rmse']                       # 14
 
-            # Get rows from individual files
-
-            if max_scans and len(profile_paths) > max_scans - num_scans:
-                profile_paths = profile_paths[:max_scans-num_scans]                
-            num_scans += len(profile_paths)
-            
-            df = util.aggregate_profiles_to_scan_level(profile_paths, lat_lon)
-
-            do_solar_elevation = True
-            if do_solar_elevation:
-                # Add solar elevation (note: much faster to do in batch at end than row-by-row)
-                solar_elev = pvlib.solarposition.spa_python(df['date'], df['lat'], df['lon'])
-                df['solar_elevation'] = solar_elev['elevation'].values
-
-                # Convert lat/lon to strings to preserve full precision --- other floats will be truncated
-                df['lat'] = df['lat'].apply(str)
-                df['lon'] = df['lon'].apply(str)
-
-                # Add solar elevation to column names and write to file
-                column_names.insert(4, 'solar_elevation')
-
+            outfile = f'{root}/scan_level/{station}-{year}.csv'
             df.to_csv(outfile, columns=column_names, index=False, float_format='%.4f')
 
             n_scans = len(df)
@@ -125,13 +141,21 @@ def aggregate_station_years_by_scan(root, stations, years, max_scans):
 
 def resample_station_years(root, stations, years, freq="5min"):
 
+    print(f"***Resampling to {freq}***")
+
+    outdir = f'{root}/{freq}'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    
     for station in stations:
         for year in years:
+
+            print(f"{station}-{year}")            
             resampled_df = util.load_and_resample_station_year(root, station, year)
 
             resampled_df.insert(3, 'date', resampled_df.index)
             
-            outfile = f'{root}/resampled/{station}_{year:4d}.csv'
+            outfile = f'{outdir}/{station}-{year:4d}-{freq}.csv'
 
             resampled_df.to_csv(outfile, 
                                 date_format='%Y-%m-%d %H:%M:%SZ',
@@ -141,14 +165,17 @@ def resample_station_years(root, stations, years, freq="5min"):
 
 def aggregate_station_years_to_daily(root, stations, years, freq='5min'):
 
+    print(f"***Aggregating to daily***")
+
     daily_data_folder = f'{root}/daily'
     if not os.path.exists(daily_data_folder):
         os.makedirs(daily_data_folder)
 
     for station in stations:
         for year in years:
+            print(f"{station}-{year}")
             df = util.aggregate_single_station_year_to_daily(root, station, year, freq=freq)
-            outfile = f'{daily_data_folder}/{station}_{year}.csv'
+            outfile = f'{daily_data_folder}/{station}-{year}-daily.csv'
             df.to_csv(outfile, index=False, float_format='%.6g')
     
                 
