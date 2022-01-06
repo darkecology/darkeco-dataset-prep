e5bc824b66b07a3e61fb4b7312260979c8ee61ec
Dan Sheldon <sheldon@cs.umass.edu>
2022-01-06 16:22:09 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cajundata',
+    version="0.0.1",
+    description='Cajun data set scripts',
+    packages=find_packages(),
+    url='https://github.com/darkecology/cajundata',
+    author='Dan Sheldon',
+    author_email='sheldon@cs.umass.edu',
+    install_requires=[
+        'pvlib',
+        'pandas>=1.0.0'
+    ],
+    keywords='radar aeroecology bird migration',
+    license='MIT'
+)
